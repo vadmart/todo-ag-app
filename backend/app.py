836d6c83f4623b274029162a2367804b7adffb66
VDM <vadik@Mac-mini-VDM.local>
2025-01-06 17:46:13 +0200

    Added logic for showing planned tasks and adding a new one on backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager, create_access_token
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import mapped_column, Mapped
@@ -17,15 +17,22 @@ jwt = JWTManager(app)
 bcrypt = Bcrypt(app)
 
 
-class Task(db.Model):
+class ModelToDictMixin(db.Model):
+    __abstract__ = True
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class Task(ModelToDictMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
-    deadline = db.Column(db.DateTime)
+    deadline = db.Column(db.DateTime, nullable=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
 
 
-class User(db.Model):
+class User(ModelToDictMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
@@ -54,13 +61,34 @@ def login():
     password = data['password']
     user = User.query.filter_by(username=username).first()
     if user and bcrypt.check_password_hash(user.password, password):
-        access_token = create_access_token(identity=user.id)
+        access_token = create_access_token(identity=str(user.id))
         return jsonify({'access': access_token, 'username': user.username})
     else:
         return jsonify({'message': 'Помилка під час входу'}), 401
 
 
+@app.route('/planned-tasks', methods=['GET'])
+@jwt_required()
+def planned_tasks():
+    user_id = get_jwt_identity()
+    tasks = Task.query.filter_by(user_id=user_id).all()
+    return jsonify(tasks)
+
+
+@app.route('/add-task', methods=['POST'])
+@jwt_required()
+def add_task():
+    user_id = get_jwt_identity()
+    data = request.get_json()
+    name = data['name']
+    deadline = data.get('deadline')
+    task = Task(name=name, completed=False, deadline=deadline, user_id=int(user_id))
+    db.session.add(task)
+    db.session.commit()
+    return jsonify(task.to_dict())
+
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        app.run(debug=True)
+        app.run(debug=True, port=8000)
